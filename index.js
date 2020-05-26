d5d1d4f50b13cb5942f16aac8eb259507a5dbd69
xgameryt92 <65968727+xgameryt92@users.noreply.github.com>
2020-05-26 20:14:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client({ disableEveryone: true });
-const prefix = '?'
-const token = 'NzEzMDczNjIxMjQ5MDMyMjgz.Xs1LcQ.HPcasSZ38wtIewnqXxxaLCeWk54'
+const Discord = require("discord.js");
+const bot = new Discord.Client({ disableEveryone: true });
+const prefix = '?'
+const token = (process.env.TOKEN) ;
 
-
-bot.on('ready', () => {
-    console.log("Bot-ul tocmai s-a pornit ca " + bot.user.tag)
-    bot.user.setActivity('Pentru ajutor mesaj in privat!');
+bot.on('ready', () => {
+    console.log("Bot-ul tocmai s-a pornit ca " + bot.user.tag)
+    bot.user.setActivity('Pentru ajutor mesaj in privat!');
 })
 
-bot.login(token); 
+bot.login(token); 
